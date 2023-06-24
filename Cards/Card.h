ca97a6ab7389be882ef9d06f42220abcce66b6b9
daniella <sdanielle@campus.technion.ac.il>
2023-06-24 20:44:08 +0300

    fixed card.h

--- Cards/Card.h
+++ Cards/Card.h
@@ -1,11 +1,68 @@
 //
 // Created by Daniella on 13/06/2023.
 //
-//TODO:
-//implement Card class and 6 card type subclasses(battle, Mana, Bar fight, Well, Treasure, Merchant)
-//create 3 subclasses for Battle(Gremlin,Witch,Dragon)
+
 
 #ifndef EX4_MTM_CARD_H
 #define EX4_MTM_CARD_H
 
+#include <string>
+#include <map>
+#include "../Players/Player.h"
+
+
+using std::string;
+
+/**
+ * CARD TYPES:
+ *
+ * MERCHANT-
+ *
+ * Treasure-
+ *
+ * Battle-
+ *
+ * Mana-
+ *
+ * Well-
+ *
+ * Well-
+ *
+ */
+
+class Card{
+protected:
+    std::string m_name;
+
+    /**
+     * C'tor
+     * @param name
+     */
+    explicit Card(const std::string& name): m_name(name){};
+public:
+
+    Card* createNewCard(const string& type);
+
+    /*
+     * handles the encounter with player
+     * each card has a different behavior
+     */
+    virtual void applyEncounter( Player&) const=0;
+
+    /*
+     * default print function
+     */
+    virtual std::ostream& print(std::ostream& os) const;
+
+    //Here we are explicitly telling the compiler to use the default/delete methods:
+    Card& operator=(Card&)=delete;
+    Card(Card&)=default;
+    virtual ~Card()=default;
+
+
+    //overloading operator >>
+    friend std::ostream& operator>>(std::ostream& os, const Card& card);
+};
+
 #endif //EX4_MTM_CARD_H
+
